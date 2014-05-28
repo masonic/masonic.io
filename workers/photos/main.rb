75723ead2d6065d0c9c8926b056671f1773a4c99
Brian Holder-Chow Lin On <brian.holderchow@gmail.com>
2014-05-28 01:53:05 -0700

    authenticate

--- workers/photos/main.rb
+++ workers/photos/main.rb
@@ -1,3 +1,4 @@
+require 'dropbox_sdk'
 
 class Datastore
   def initialize
@@ -24,10 +25,47 @@ class PhotoPool
   end
 end
 
+def get_auth_token_interactive key, secret
+  flow = DropboxOAuth2FlowNoRedirect.new(key, secret)
+  authorize_url = flow.start()
+
+  # Interactively prompt
+  puts '1. Go to: ' + authorize_url
+  puts '2. Click "Allow" (you might have to log in first)'
+  puts '3. Copy the authorization code'
+  print 'Enter the authorization code here: '
+  code = gets.strip
+
+  access_token, _ = flow.finish(code)
+  return access_token
+end
+
+def get_auth_token interactive=false
+  if interactive
+    # TODO bubble up errors
+    key = ENV['DBX_MASONIC_MEMORIES_KEY']
+    secret = ENV['DBX_MASONIC_MEMORIES_SECRET']
+
+    while true
+      begin
+        access_token = get_auth_token_interactive(key, secret)
+      rescue DropboxError
+        next
+      ensure
+        return get_auth_token_interactive key, secret
+      end
+    end
+
+  else
+    return ENV['DBX_MASONIC_MEMORIES_DEV_TOKEN']
+  end
+end
+
 def main
 
-  key = ENV['DBX_MASONIC_MEMORIES_KEY']
-  secret = ENV['DBX_MASONIC_MEMORIES_SECRET']
+  access_token = get_auth_token
+
+  client = DropboxClient.new(access_token)
 
   datastore = Datastore.new
 
