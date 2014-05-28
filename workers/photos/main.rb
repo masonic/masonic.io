7fb1a98f23f8e39c2429a429c6afc717770c6f04
Brian Holder-Chow Lin On <brian.holderchow@gmail.com>
2014-05-28 01:13:15 -0700

    [photos] bake the donut. next: fill.

--- workers/photos/main.rb
+++ workers/photos/main.rb
@@ -1,5 +1,42 @@
 
+class Datastore
+  def initialize
+    # TODO auth AWS S3
+  end
+
+  def put
+    nil
+  end
+end
+
+class PhotoPool
+  def initialize
+    # TODO authenticate dropbox api
+  end
+
+  def each &block
+    # TODO access data in folder
+    # TODO yield photo
+  end
+
+  def remove photo
+    # TODO delete from dropbox
+  end
+end
+
 def main
+
+  pool = PhotoPool.new
+  datastore = Datastore.new
+
+  pool.each do |photo|
+    err = datastore.put(photo)
+    if err == nil then
+      pool.remove(photo)
+    else
+      # TODO maybe retry
+    end
+  end
 end
 
 main()
